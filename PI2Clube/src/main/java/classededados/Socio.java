105bb63129d5368eb05892fd9a3b5a6ddbb8e4c6
100883 <guifreitas89@live.com>
2016-11-16 17:14:06 -0200

    Implementação da classe socio

--- PI2Clube/src/main/java/classededados/Socio.java
+++ PI2Clube/src/main/java/classededados/Socio.java
@@ -11,6 +11,47 @@ package classededados;
  *            Guilherme freitas
  *            Vinicius Caetano
  */
-public class Socio {
+abstract class Socio {
     
+    //Atribútos
+    protected String nome = "";
+    protected String endereco = "";
+    protected int fone = 0;
+    protected String email = "";
+    
+    //Métodos    
+    public Socio(String nome, String endereco, int fone, String email){
+        super();
+        this.nome = nome;
+        this.endereco = endereco;
+        this.fone = fone;
+        this.email = email;
+    }
+    
+    public String getNome() {
+        return nome;
+    }
+    public String getEndereco() {
+        return endereco;
+    }
+    public int getFone() {
+        return fone;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+    public void setFone(int fone) {
+        this.fone = fone;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }    
+ 
 }
+
