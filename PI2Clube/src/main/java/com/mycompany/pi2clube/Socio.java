8bb59e681cffac63b3218147aca8f21e44e8aa5a
100883 <guifreitas89@live.com>
2016-11-16 16:50:59 -0200

    Classe de dados socio

---
+++ PI2Clube/src/main/java/com/mycompany/pi2clube/Socio.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.pi2clube;
+
+/**
+ *
+ * @authores  Jeferson Gustavo
+ *            Guilherme freitas
+ *            Vinicius Caetano
+ */
+public class Socio {
+    
+}
